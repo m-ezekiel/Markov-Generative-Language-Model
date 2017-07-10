95d3c27a0ec825fc239b814ebd1799dd49bf7c7d
m-ezekiel <git@m-ezekiel.com>
2017-07-10 01:34:16 -0500

    Add wordVec output support for bigrams and trigrams.

--- corpusToVector_fxn.R
+++ corpusToVector_fxn.R
@@ -2,13 +2,26 @@
 # Desc: Takes an input file (.txt) and returns a string vector separated by whitespace.
 # 2017-07-08
 
-corpusToVector <- function(file) {
+corpusToVector <- function(file, nGram = 1) {
 
   text_data <- readLines(file)
   gsub("[()_!]", "", text_data) -> text_data; 
   gsub("--", " ", text_data) -> text_data
   wordVec <- unlist(strsplit(text_data, split = " "))
+
+  bigrams <- NULL
+  trigrams <- NULL
+    
+  if (nGram == 2) {
+    for (i in 1:length(wordVec)-1)
+      bigrams[i] <- paste0(wordVec[i], " ", wordVec[i+1])
+    wordVec <- bigrams
+  }
+  if (nGram == 3) {
+    for (i in 1:length(wordVec)-2)
+      trigrams[i] <- paste0(wordVec[i], " ", wordVec[i+1], " ", wordVec[i+2])
+    wordVec <- trigrams
+  }
   
   return(wordVec)
-
 }
