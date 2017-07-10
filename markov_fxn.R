ac23215c969f8cac00081bc7f0e5e1facc1e4e2f
m-ezekiel <git@m-ezekiel.com>
2017-07-09 20:19:28 -0500

    Add support for higher order/degree markov models (depends on shape of input data).

--- markov_fxn.R
+++ markov_fxn.R
@@ -11,21 +11,33 @@ markov_fxn <- function(n = 30, begin_with = "", wordVec = wordVector) {
   beginning <- wordVec[grep(begin_with, wordVec)]
   sample(beginning, 1) -> context; 
   emptyArray[1] <- context;
-  ## print(context) 
+#  print(context) 
   
   for(i in 1:n) {
     sample(wordVec[which(wordVec == context) + 1], size = 1) -> context; 
     emptyArray[i+1] <- context;
-    ## print(context)
+#    print(context)
     if (grepl("[.]$", context) == TRUE)
       break
   }
   
   filledArray <- na.exclude(emptyArray)
   machineText <- ""
+    
+  # Define output index based on order and degree of the Markov model.
+  # This is a brittle solution, the shape of the data should dictate the model behavior.
+  dataShape <- length(unlist(strsplit(wordVec[1], split = " ")))
+  print(dataShape)
+  if (dataShape == 1)
+    index <- seq(from=1, to=length(filledArray), 1)
+  if (dataShape == 2)
+    index <- seq(from=1, to=length(filledArray), 2)
+  if (dataShape == 3)
+    index <- seq(from=1, to=length(filledArray), 3)
   
-  for (i in 1:length(filledArray)) {
-    machineText <- paste0(machineText, filledArray[i], " ")
+  # Append output text to a single string vector
+  for (i in 1:length(index)) {
+    machineText <- paste0(machineText, filledArray[index[i]], " ")
   }
   
   return(machineText)
