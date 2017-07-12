0408bc5fc406a7ed69825a4fee31a7a683d0ea47
m-ezekiel <git@m-ezekiel.com>
2017-07-12 16:35:01 -0500

    Add fxn to return top n word frequencies after stopword removal and unified case.

---
+++ wordFreq_fxn.R
@@ -0,0 +1,22 @@
+# File: wordFreq_fxn.R
+# Desc: Returns top N words from a text file.
+# Created: 2017-07-12 by @m_ezkiel
+
+# require('stringr')
+
+wordFreq <- function(file = "", n = 10, dict = "stopWords_english.txt") {
+  library('stringr')
+  myStops <- suppressWarnings(corpusToVector(dict))
+  wordVector <- suppressWarnings(corpusToVector(file, removePunct = TRUE))
+  cleanWords <- str_to_lower(wordVector)
+  
+  for (i in 1:length(myStops)) 
+    cleanWords <- cleanWords[cleanWords != myStops[i]]
+  
+  top_N <- tail(sort(rowSums(as.matrix(table(cleanWords)))), 10)
+  
+  return(top_N)
+}
+
+# Sample use case
+# wordFreq("data/time_machine_Wells.txt")
